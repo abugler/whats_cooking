f541e077fa0dff1224af206058507db41230ec6a
abugler <abugler@gmail.com>
2020-02-27 19:24:14 -0600

    updated ascii

--- nn.py
+++ nn.py
@@ -37,13 +37,15 @@ class CuisineClassifier(nn.Module):
     def forward(self, X):
         return self.output(self.hidden(self.input(X)))
 
-output_template = "#############################\n" + \
-                  "#                           #\n" + \
-                  "#        Epoch: {3:3}         #\n" + \
-                  "#   Time Elapsed:  {0:2}:{1:2}    #\n" + \
-                  "# Validation Loss:  {2:7.5f} #\n" + \
-                  "#                           #\n" + \
-                  "#############################"
+output_template = "#################################\n" + \
+                  "#                               #\n" + \
+                  "#          Epoch: {4:3}           #\n" + \
+                  "#     Time Elapsed:  {0:2}:{1:2}      #\n" + \
+                  "#      Total Time:  {5:2}:{6:2}       #\n" + \
+                  "#   Validation Loss:  {2:7.5f}   #\n" + \
+                  "#  Min Training Loss:  {3:7.5f}  #\n" + \
+                  "#                               #\n" + \
+                  "#################################"
 
 def train(model, training_features, training_targets,
           validation_features, validation_targets):
@@ -58,12 +60,13 @@ def train(model, training_features, training_targets,
     validation_targets = torch.from_numpy(validation_targets).long().cuda()
 
     min_valid_loss = 1e10
-
+    train_start = int(time.time())
     for epoch in range(model.n_epochs):
         epoch_start = int(time.time())
         print(f"Starting Epoch {epoch}")
         # Do mini-batch gradient descent
         perm = torch.randperm(training_features.size()[0])
+        min_training_loss = 1e10
         for i in range(0, training_features.size()[0], batch_size):
             optimizer.zero_grad()
             # Find batches
@@ -77,16 +80,20 @@ def train(model, training_features, training_targets,
             # backprop
             batch_loss.backward()
             optimizer.step()
+            if batch_loss.item() < min_training_loss:
+                min_training_loss = batch_loss.item()
+
         # validation loss
-        valid_loss = loss(model(validation_features), validation_targets)
+        valid_loss = loss(model(validation_features), validation_targets).item()
         if min_valid_loss > valid_loss:
             min_valid_loss = valid_loss
             torch.save(model, "best_nn")
         epoch_end = int(time.time())
-        seconds_elapsed = epoch_end - epoch_start
-
+        epoch_elapsed = epoch_end - epoch_start
+        train_elapsed = epoch_end - train_start
         print(output_template.format(
-            int(seconds_elapsed / 60), seconds_elapsed % 60, valid_loss.item(), epoch
+            int(epoch_elapsed / 60), epoch_elapsed % 60, valid_loss, min_training_loss, epoch,
+            int(train_elapsed / 60), train_elapsed % 60
         ))
 
 
