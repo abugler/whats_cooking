54d1fe12621803c9dfac6dc88e28d8a7180a0517
abugler <abugler@gmail.com>
2020-02-27 18:37:09 -0600

    ready for nn training

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ fit_prior = True
 uniform_prior = False
 
 # For Artificial Neural Network
-n_layers = 1
-n_epochs = 2
-n_hidden = [1000]
+n_layers = 3
+n_epochs = 100
+n_hidden = [1000, 500, 200]
 lr = .003
 momentum = .8
 batch_size = 100
